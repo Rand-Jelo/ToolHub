64b0831f8029cde538aeaa992a2ae41240523d1e
ClamiestGalaxy <162509575+ClamiestGalaxy@users.noreply.github.com>
2025-05-08 13:08:50 +0200

    Update secret key to be hidden

--- toolhub/settings.py
+++ toolhub/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f+4(_ob9e$n%#i&&re32y=#2o@73-i)nj9xa@))2rm)yln8v8r'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
